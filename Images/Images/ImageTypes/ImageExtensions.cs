efd9165f8670082a1cf9b705a42e1f43d58b2dcd
Jens Steenmetz <jj.steenmetz@gmail.com>
2023-11-13 03:23:07 +0100

    Add point operations
    Add two types of point operations:
    - Point operations returning an image of the same type.
    - Point operations returning an image with a different pixel type.

---
+++ Images/Images/ImageTypes/ImageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Images
+{
+    public static class ImageExtensions
+    {
+        public static T ApplyPointOperation<T, U>(this T image, Func<U, U> pointOperation)
+            where T : Image<U>, new()
+            => image.ApplyPointOperation<T, U, T, U>(pointOperation);
+
+        public static V ApplyPointOperation<T, U, V, W>(this T image, Func<U, W> pointOperation)
+            where T : Image<U> where V : Image<W>, new()
+        {
+            V tempImage = Image<W>.CreateEmpty<V>(image.Dimensions);
+
+            Parallel.For(0, image.PixelCount, i => tempImage[i] = pointOperation(image[i]));
+
+            return tempImage;
+        }
+    }
+}
