24712240b1cb2a5aa3a076283c27f32a8396ea38
Jens Steenmetz <jj.steenmetz@gmail.com>
2023-11-13 03:10:32 +0100

    Add Image and Image2D classes
    The Image<T> and Image2D<T> provide generic image classes.
    
    They contain pixel data and basic image manipulation syntax, such as:
    - Indexers
    - Casting between similar image types
    - Instance creation
    - IEnumerators
    - Size checks

---
+++ Images/Images/ImageTypes/Image.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+
+namespace Images
+{
+    public class Image<T> : IEnumerable<T>, IEnumerable, ICloneable
+    {
+        protected T[] _data;
+
+        public int PixelCount { get; protected set; }
+
+        public int NumberOfDimensions { get; protected set; }
+
+        public int[] Dimensions { get; protected set; }
+
+        /// <summary>
+        /// Warning: do not use! Useful for using generic type constraint 'new()' within this and parent classes.
+        /// </summary>
+        public Image()
+        {
+            Dimensions = Array.Empty<int>();
+            NumberOfDimensions = 0;
+            PixelCount = 0;
+
+            _data = Array.Empty<T>();
+        }
+
+        public Image(Image<T> image) : this(image.Dimensions)
+            => Array.Copy(image._data, _data, _data.Length);
+
+        public Image(params int[] dimensions)
+        {
+            Dimensions = dimensions;
+            NumberOfDimensions = dimensions.Length;
+            PixelCount = dimensions.Aggregate((a, b) => a * b);
+
+            _data = new T[PixelCount];
+        }
+
+        public virtual T this[int index]
+        {
+            get => _data[index];
+            set => _data[index] = value;
+        }
+
+        public virtual T this[params int[] coordinates]
+        {
+            get => _data[GetIndex(coordinates)];
+            set => _data[GetIndex(coordinates)] = value;
+        }
+
+        protected virtual int GetIndex(params int[] coordinates)
+        {
+            if (coordinates.Length != NumberOfDimensions)
+                throw new ArgumentOutOfRangeException(nameof(coordinates));
+
+            int sum = 0, step = 1;
+
+            for (int i = 0; i < NumberOfDimensions; i++)
+            {
+                sum += step * coordinates[i];
+                step *= Dimensions[i];
+            }
+
+            return sum;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+            => ((IEnumerable<T>)_data).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => _data.GetEnumerator();
+
+        public virtual Image<T> Copy()
+            => new(this);
+
+        public virtual object Clone()
+            => Copy();
+
+        public static U CreateEmpty<U>(params int[] dimensions) where U : Image<T>, new()
+        {
+            int pixelCount = dimensions.Aggregate((a, b) => a * b);
+
+            U image = new()
+            {
+                Dimensions = dimensions,
+                NumberOfDimensions = dimensions.Length,
+                PixelCount = pixelCount,
+                _data = new T[pixelCount]
+            };
+
+            return image;
+        }
+    }
+}
---
+++ Images/Images/ImageTypes/Image2D.cs
@@ -0,0 +1,64 @@
+﻿namespace Images
+{
+    public abstract class Image2D<T> : Image<T>
+    {
+        public virtual int Width => Dimensions[0];
+        public virtual int Height => Dimensions[1];
+        public (int, int) Size => (Width, Height);
+
+        /// <summary>
+        /// Warning: do not use! Useful for using generic type constraint 'new()' within this and parent classes.
+        /// </summary>
+        public Image2D() : base() { }
+
+        public Image2D(Image2D<T> image) : base(image) { }
+
+        public Image2D(int width, int height) : base(width, height) { }
+
+        public T this[(int x, int y) coordinates]
+        {
+            get => this[coordinates.x, coordinates.y];
+            set => this[coordinates.x, coordinates.y] = value;
+        }
+
+        public virtual T this[int x, int y]
+        {
+            get => _data[GetIndex(x, y)];
+            set => _data[GetIndex(x, y)] = value;
+        }
+
+        private int GetIndex(int x, int y)
+            => y * Width + x;
+
+        protected static V Cast<U, V>(U image) where U : Image2D<T> where V : Image2D<T>, new()
+        {
+            V tempImage = new()
+            {
+                Dimensions = image.Dimensions,
+                NumberOfDimensions = image.NumberOfDimensions,
+                PixelCount = image.PixelCount,
+                _data = image._data
+            };
+
+            return tempImage;
+        }
+
+        public override Image<T> Copy()
+            => new(this);
+
+        public bool IsSameSizeAs(Image2D<T> image)
+            => this.Size == image.Size;
+
+        public static bool AreSameSize(params Image2D<T>[] images)
+        {
+            if (images.Length == 0) return true;
+
+            (int, int) size = images[0].Size;
+
+            foreach (Image2D<T> image in images)
+                if (image.Size != size) return false;
+
+            return true;
+        }
+    }
+}
